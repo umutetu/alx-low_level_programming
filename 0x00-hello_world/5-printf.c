7b7ae67d462dfef40b36ee81b24c179f015503a2
zebiba <umutetu@gmail.com>
2022-12-08 15:01:33 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
  * main - myfunction
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
-	    return (0);
+	printf(with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 
 }
+
