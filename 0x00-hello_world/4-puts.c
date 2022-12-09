6836506933f9594cc4a4dfab42691c4d7f6e7a0d
zebiba <umutetu@gmail.com>
2022-12-08 20:57:20 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  * Return: 0 value
  */
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
