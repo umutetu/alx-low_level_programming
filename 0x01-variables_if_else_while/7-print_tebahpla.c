2483f434ecf4653a7818ecf9842f68e419857b9f
zebiba <umutetu@gmail.com>
2022-12-12 13:53:55 -0800

    base16

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,48 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - function to print numbers between 00 to 99.
+ * main - prints the lowercase alphabet in reverse.
  *
- * Return: 0 to success
+ * Return: zero value
  */
 int main(void)
 {
-int i, j, k, l, d1, d2;
+	int i;
 
-i = j = k = l = 48;
-while (l < 58)
-{
-	k = 48;
-	while (k < 58)
+	for (i = 122; i > 96; i--)
 	{
-	j = 48;
-	while (j < 58)
-	{
-	i = 48;
-	while (j < 58)
-	{
-	d1 = (l * 10) + j;
-	d2 = (j * 10) + i;
-	if (d1 < d2)
-	{
-	putchar(l);
-	putchar(j);
-	putchar(' ');
-	putchar(j);
 	putchar(i);
-	if (l == 57 && j == 56 && j == 57 && i == 57)
-	break;
-	putchar(',');
-	putchar(' ');
 	}
-	i++;
-	}
-	j++;
-	}
-	k++;
-	}
-	l++;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
