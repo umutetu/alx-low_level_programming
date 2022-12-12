3f089d93db9c2d11e9d399f5d2d86947f56630b8
zebiba <umutetu@gmail.com>
2022-12-12 00:54:36 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- *main - my function
- * 
+ * main - my function
+ *
  * Return: 0 value
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
