24ecc24d1bc8be4ea976c1719b711ab5ff1fbe2f
zebiba <umutetu@gmail.com>
2022-12-11 21:13:23 -0800

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function print numbers from 0 to 9.
+ *
+ * Return: 0 value
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
