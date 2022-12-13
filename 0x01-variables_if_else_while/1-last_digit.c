4d0abc67b97db34db08abbda45badb37ce752297
zebiba <umutetu@gmail.com>
2022-12-12 18:02:04 -0800

    print last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,24 +11,24 @@
  */
 int main(void)
 {
-	int n;
+	int n, i;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if ((n % 10) > 5)
+	i = n % 10;
+	if (i > 5)
 	{
 		printf("last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
+			n, i);
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
+	else if (i == 0)
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
+			n, i);
 	}
 	else
 	{
-		printf("last digit of %d is %d and is 0\n", n, n % 10);
+		printf("last digit of %d is %d and is 0\n", n, i);
 	}
 	return (0);
 
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse.
+ *
+ * Return:0 value 
+ */
+int main(void)
+{
+	int i
+
+	for (i = 122; i > 9; i > 96; i--)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
