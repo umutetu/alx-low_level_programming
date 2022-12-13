bac8c5907bf68dd9c8ca54c0c6da7003cde913f6
zebiba <umutetu@gmail.com>
2022-12-12 18:22:04 -0800

    print last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,8 +18,7 @@ int main(void)
 	i = n % 10;
 	if (i > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n",
-			n, i);
+		printf("last digit of %d is %d and is greater than 5\n", n, i);
 	}
 	else if (i == 0)
 	{
