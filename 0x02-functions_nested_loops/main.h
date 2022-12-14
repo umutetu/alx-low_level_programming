6f4baabae72e6f3381d9505eaa945ffa7798fd9e
zebiba <umutetu@gmail.com>
2022-12-14 13:11:57 -0800

    print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef _main_h_
+#define _main_h_
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +13,4 @@ void times_tablet(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_tablet(int n);
+#endif
