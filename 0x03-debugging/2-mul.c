b833325da30cfbfa6e9900bf3db870dc58f4796a
zebiba <umutetu@gmail.com>
2022-12-15 14:51:07 -0800

    multiply

--- 0x03-debugging/2-mul.c
+++ 0x03-debugging/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * mul - function to multiplying two numbers.
- *   * @num1: first number.
- *    * @num2: second number.
- *     * Return: multiplication.
- *      */
+ * mul - function to multiplying two numbers.
+ * @num1: first number.
+ * @num2: second number.
+ * Return: multiplication.
+ */
 int mul(int num1, int num2)
 {
 		return (num1 * num2);
