82f9a2ff6769868c87179f7d80a4e3000dd8b915
zebiba <umutetu@gmail.com>
2022-12-15 15:20:23 -0800

    function to print number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print numbers
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if (n != 50 && n != 52)
+			_putchar(n);
+	}
+	_putchar('\n');
+}
