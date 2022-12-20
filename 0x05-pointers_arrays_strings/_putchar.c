125783768e8da28f4b6839d4188891890207ce52
zebiba <umutetu@gmail.com>
2022-12-19 16:09:36 -0800

    half

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* _putchar - writes the character c to standout
+* @c: the character to print
+* Return: on success 1.
+* on error, -1 is returned, and errno is set appropriatly.
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
