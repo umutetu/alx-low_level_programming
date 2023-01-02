4aa2b28bf7388b5767802331a327c3b39dcf080a
zebiba <umutetu@gmail.com>
2023-01-02 07:52:40 -0800

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_ 
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+#endif
