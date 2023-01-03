01ad8a966c22ffb52f3fa8f918b4f26700b20abb
zebiba <umutetu@gmail.com>
2023-01-03 05:04:00 -0800

    string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,4 +13,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+void set_string(char **s, char *to);
 #endif
